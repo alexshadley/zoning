4445287422d9f755299f204452435525d477e10e
Alex Shadley <shadleyalex@gmail.com>
2025-01-06 07:49:36 -0800

    move parcels geojson out of initial page load

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,28 @@
-import Parcels from "../data/prc_hgt_bldg.geo.json";
+"use client";
 import NhoodGeoms from "../data/nhoods.geo.json";
 import { MainApp } from "./MainApp";
 import { Parcels as ParcelsType } from "./ParcelMap";
 import { FeatureCollection, MultiPolygon } from "geojson";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [parcels, setParcels] = useState<ParcelsType | null>(null);
+
+  useEffect(() => {
+    // Load large GeoJSON file client-side
+    import("../data/prc_hgt_bldg.geo.json").then((data) =>
+      setParcels(data as unknown as ParcelsType)
+    );
+  }, []);
+
+  if (!parcels) {
+    // nice loading state
+    return <div>Loading map data...</div>;
+  }
+
   return (
     <MainApp
-      parcels={Parcels as ParcelsType}
+      parcels={parcels}
       nhoodGeoms={
         NhoodGeoms as FeatureCollection<MultiPolygon, { nhood: string }>
       }
